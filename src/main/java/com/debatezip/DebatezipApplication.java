6bc07a00a85fd6869d27df01422a26ee8f5ba34d
leegwichan <cnddkscndgus@naver.com>
2026-05-10 16:59:21 +0900

    init: 기본 설정 진행

---
+++ src/main/java/com/debatezip/DebatezipApplication.java
@@ -0,0 +1,13 @@
+package com.debatezip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebatezipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebatezipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/debatezip/DebatezipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.debatezip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebatezipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
